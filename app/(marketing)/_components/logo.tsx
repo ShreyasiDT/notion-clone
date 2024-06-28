20ba1ddc34b1fa79148df8d03f860be81645aaf8
ShreyasiDT <shreyasit1792@gmail.com>
2024-06-28 15:37:26 +0530

    initial landing page

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src="/logoBright1.png" height="40" width="40" alt="Logo" />
+      <p className={cn("font-semibold", font.className)}>Notely</p>
+    </div>
+  );
+};
