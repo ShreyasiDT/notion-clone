953c3c94ca9adcb689c0cc839ea59fa7f8d23b91
ShreyasiDT <shreyasit1792@gmail.com>
2024-08-05 15:54:40 +0530

    banner component

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 interface NavbarProps {
   isCollapsed: boolean;
   onResetWidth: () => void;
@@ -21,10 +23,13 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   if (document === undefined) {
     return (
       <nav
-        className="bg-background dark-[#1F1F1F] px-3 py-2 w-full
-        flex items-center"
+        className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full
+        flex items-center justify-between"
       >
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -34,7 +39,7 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
   return (
     <>
       <nav
-        className="bg-background dark:bg-[#1F1F1F] px-3 w-full
+        className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full
     flex items-center gap-x-4"
       >
         {isCollapsed && (
@@ -44,10 +49,14 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
             className="h-6 w-6 text-muted-foreground"
           />
         )}
-        <div className="flex items-center justify-between wf-ull">
+        <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
